0ec046c504ea5333587037b88c8620231c47d909
Truyen-IT <truyenb1910323@student.ctu.edu.vn>
2022-10-30 21:35:56 +0700

    Luu san pham yeu thich len Realtime Database cua Firebase

--- lib/ui/products/product_grid_tile.dart
+++ lib/ui/products/product_grid_tile.dart
@@ -3,31 +3,29 @@ import 'package:provider/provider.dart';
 import '../../models/product.dart';
 import '../cart/cart_manager.dart';
 import 'product_detail_screen.dart';
+import 'products_manager.dart';
+
 class ProductGriTile extends StatelessWidget {
   const ProductGriTile(
-    this.product,{
-      super.key,
-    }
-  );
-  final Product product;//lien ket tu day ra
-  
+    this.product, {
+    super.key,
+  });
+  final Product product; //lien ket tu day ra
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(10),//bo tron
+      borderRadius: BorderRadius.circular(10), //bo tron
       child: GridTile(
         footer: buildGridFooterBar(context),
         child: GestureDetector(
-          onTap: (){
+          onTap: () {
             Navigator.of(context).pushNamed(
-            ProductDetailScreen.routeName,
-             arguments: product.id,
-            
+              ProductDetailScreen.routeName,
+              arguments: product.id,
             );
-           
           },
-           child: GestureDetector(
+          child: GestureDetector(
             onTap: () {
               Navigator.of(context).push(
                 MaterialPageRoute(
@@ -35,75 +33,68 @@ class ProductGriTile extends StatelessWidget {
                 ),
               );
             },
-          child:Image.network(
-            product.imageUrl,
-            fit:BoxFit.cover,
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            ),
           ),
         ),
-       ),
-    ),
+      ),
     );
-    
   }
-Widget buildGridFooterBar(BuildContext context){
-  return GridTileBar(
-    backgroundColor: Colors.black87,
-    // leading: IconButton(
-    //   icon:Icon(
-    //     product.isFavorite ? Icons.favorite : Icons.favorite_border,
-    //     ),
-    leading: ValueListenableBuilder<bool>(
-       valueListenable: product.isFavoriteListenable,
-       builder: (ctx, isFavorite, child) {
-         return IconButton(
-           icon: Icon(
+
+  Widget buildGridFooterBar(BuildContext context) {
+    return GridTileBar(
+      backgroundColor: Colors.black87,
+      // leading: IconButton(
+      //   icon:Icon(
+      //     product.isFavorite ? Icons.favorite : Icons.favorite_border,
+      //     ),
+      leading: ValueListenableBuilder<bool>(
+        valueListenable: product.isFavoriteListenable,
+        builder: (ctx, isFavorite, child) {
+          return IconButton(
+            icon: Icon(
               isFavorite ? Icons.favorite : Icons.favorite_border,
-    ),
-
-   
-    
-    
-     color:Theme.of(context).colorScheme.secondary,
-     onPressed: (){
-      product.isFavorite = !isFavorite;
-
-     },
-         );
-       },
-     ),
-    title:Text(
-      product.title,
-      textAlign:TextAlign.center,
-  ),
-  trailing:IconButton(icon: const Icon(
-    Icons.shopping_cart,
-  ),
-  onPressed: (){
-   final cart = context.read<CartManager>();
-   cart.addItem(product);
-   ScaffoldMessenger.of(context)
-     ..hideCurrentSnackBar()
-     ..showSnackBar(
-       SnackBar(
-         content: const Text(
-           'Item added to cart',
-         ),
-       duration: const Duration(seconds: 2),
-       action: SnackBarAction(
-        label: 'UNDO',
-       onPressed: () {
-        cart.removeSingleItem(product.id!);
-      },
-   ),
- ),
- );
-
-
-  },
-  color: Theme.of(context).colorScheme.secondary,
-  ),
-
-);
-
-}
+            ),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {
+              ctx.read<ProductsManager>().tonggleFavoriteStatus(product);
+              //product.isFavorite = !isFavorite;
+            },
+          );
+        },
+      ),
+      title: Text(
+        product.title,
+        textAlign: TextAlign.center,
+      ),
+      trailing: IconButton(
+        icon: const Icon(
+          Icons.shopping_cart,
+        ),
+        onPressed: () {
+          final cart = context.read<CartManager>();
+          cart.addItem(product);
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(
+                content: const Text(
+                  'Item added to cart',
+                ),
+                duration: const Duration(seconds: 2),
+                action: SnackBarAction(
+                  label: 'UNDO',
+                  onPressed: () {
+                    cart.removeSingleItem(product.id!);
+                  },
+                ),
+              ),
+            );
+        },
+        color: Theme.of(context).colorScheme.secondary,
+      ),
+    );
+  }
 }
