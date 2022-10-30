3563dd2183cc9625b65c9d430a5f36f77a687779
Truyen-IT <truyenb1910323@student.ctu.edu.vn>
2022-10-30 13:38:34 +0700

    Cai dat chung thuc voi Firebase Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:myshop/ui/products/products_overview_screen.dart';
 import 'package:myshop/ui/products/products_manager.dart';
 import 'ui/screens.dart';
@@ -9,7 +10,9 @@ import 'package:provider/provider.dart';
 // import 'ui/products/user_products_screen.dart';
 // import 'ui/cart/cart_screen.dart';
 // import 'ui/orders/orders_screen.dart';
-void main() {
+// 
+Future<void> main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
 
@@ -22,6 +25,9 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
          ChangeNotifierProvider(
+          create: (context) => AuthManager(),
+        ),
+         ChangeNotifierProvider(
         create: (ctx) => ProductsManager(),
         ),
          ChangeNotifierProvider(
@@ -31,73 +37,88 @@ class MyApp extends StatelessWidget {
         create: (ctx) => OrdersManager(),
         ),
       ],
-    child: MaterialApp(
-      title: 'xin chao',//nó ghi trên phần mêm khung trên 
-      theme: ThemeData(//bao gồm màu cho phần chính
-        fontFamily:'Lato',
-        colorScheme: ColorScheme.fromSwatch(
-           primarySwatch: Colors.red,
-       ).copyWith(
-        secondary: Colors.deepOrange,
-       ),
-    ),
+    child: Consumer<AuthManager>(
       
-      home: const ProductsOverviewScreen(),
-      routes: {
-         CartScreen.routeName:
-            (ctx) => const CartScreen(),
-         OrdersScreen.routeName:
-            (ctx) => const OrdersScreen(),
-         UserProductsScreen.routeName:
-            (ctx) => const UserProductsScreen(),
-      },
-      onGenerateRoute: (settings) {
-         if (settings.name == ProductDetailScreen.routeName) {
-             final productId = settings.arguments as String;
-             return MaterialPageRoute(
-                builder: (ctx) {
-                   return ProductDetailScreen(
-                    ctx.read<ProductsManager>().findById(productId)
-                      // ProductsManager().findById(productId),
-                   );
+      builder: (context, authManager, child) {
+        return MaterialApp(
+          title: 'xin chao',//nó ghi trên phần mêm khung trên 
+          theme: ThemeData(//bao gồm màu cho phần chính
+            fontFamily:'Lato',
+            colorScheme: ColorScheme.fromSwatch(
+               primarySwatch: Colors.red,
+           ).copyWith(
+            secondary: Colors.deepOrange,
+           ),
+        ),
+          
+          //home: const ProductsOverviewScreen(),
+           home: authManager.isAuth
+              ? const ProductsOverviewScreen()
+              : FutureBuilder(
+                  future: authManager.tryAutoLogin(),
+                  builder: (context, snapshot) {
+                    return snapshot.connectionState == ConnectionState.waiting
+                        ? const SplashScreen()
+                        : const AuthScreen();
+                  },
+                ),
+          routes: {
+             CartScreen.routeName:
+                (ctx) => const CartScreen(),
+             OrdersScreen.routeName:
+                (ctx) => const OrdersScreen(),
+             UserProductsScreen.routeName:
+                (ctx) => const UserProductsScreen(),
+          },
+          onGenerateRoute: (settings) {
+             if (settings.name == ProductDetailScreen.routeName) {
+                 final productId = settings.arguments as String;
+                 return MaterialPageRoute(
+                    builder: (ctx) {
+                       return ProductDetailScreen(
+                        ctx.read<ProductsManager>().findById(productId)
+                          // ProductsManager().findById(productId),
+                       );
 
 
-                },
-            );
-         }
-         if (settings.name == EditProductScreen.routeName) {
-              final productId = settings.arguments as String?;
-              return MaterialPageRoute(
-                builder: (ctx) {
-                  return EditProductScreen(
-                    productId != null
-                        ? ctx.read<ProductsManager>().findById(productId)
-                        : null,
+                    },
+                );
+             }
+             if (settings.name == EditProductScreen.routeName) {
+                  final productId = settings.arguments as String?;
+                  return MaterialPageRoute(
+                    builder: (ctx) {
+                      return EditProductScreen(
+                        productId != null
+                            ? ctx.read<ProductsManager>().findById(productId)
+                            : null,
+                      );
+                    },
                   );
-                },
-              );
-            }
-            return null;
-       
-      
-      
-      // home:const SafeArea(
-      //   child:UserProductsScreen(),
-
-       
-      //  child:ProductDetailScreen(
-      //     ProductManager().items[0],
-
-      //  ),
+                }
+                return null;
            
-           // ProductManager().items[0],
-       // child: OrdersScreen(),
-        //child:CartScreen(),
-        //UserProductsScreen(),
-        //ProductsOverviewScreen(),//lien ket voi producoverviewsriem
-       //ProductDetailScreen()//,ProductManager().items[2],
-      // ProductsOverviewScreen()
-      },
+          
+          
+          // home:const SafeArea(
+          //   child:UserProductsScreen(),
+
+           
+          //  child:ProductDetailScreen(
+          //     ProductManager().items[0],
+
+          //  ),
+               
+               // ProductManager().items[0],
+           // child: OrdersScreen(),
+            //child:CartScreen(),
+            //UserProductsScreen(),
+            //ProductsOverviewScreen(),//lien ket voi producoverviewsriem
+           //ProductDetailScreen()//,ProductManager().items[2],
+          // ProductsOverviewScreen()
+          },
+        );
+      }
     ),
     );
   }
--- lib/ui/shared/app_drawer.dart
+++ lib/ui/shared/app_drawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../orders/orders_screen.dart';
 import '../products/user_products_screen.dart';
+import 'package:provider/provider.dart';
+import '../auth/auth_manager.dart';
 
 
 class AppDrawer extends StatelessWidget {
@@ -41,17 +43,17 @@ class AppDrawer extends StatelessWidget {
                   .pushReplacementNamed(UserProductsScreen.routeName);
             },
           ),
-          // const Divider(),
-          // ListTile(
-          //   leading: const Icon(Icons.exit_to_app),
-          //   title: const Text('Logout'),
-          //   onTap: () {
-          //     Navigator.of(context)
-          //       ..pop()
-          //       ..pushReplacementNamed('/');
-          //     context.read<AuthManager>().logout();
-          //   },
-          // ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Logout'),
+            onTap: () {
+              Navigator.of(context)
+                ..pop()
+                ..pushReplacementNamed('/');
+              context.read<AuthManager>().logout();
+            },
+          ),
         ],
       ),
     );
